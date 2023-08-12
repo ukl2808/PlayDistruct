d53d9367c6211d90d9e901f5ee2514acd8328afb
Admin <Admin@DESKTOP-047GBOQ>
2023-08-12 11:45:43 +0300

    Добавьте файлы проекта.

---
+++ PlayDistruct/Program.cs
@@ -0,0 +1,7 @@
+﻿Play play = new Play("Hamlet", "William Shakespeare", "Tragedy", 1600);
+
+Console.WriteLine("Play Information:");
+Console.WriteLine($"Title: {play.Title}");
+Console.WriteLine($"Author: {play.Author}");
+Console.WriteLine($"Genre: {play.Genre}");
+Console.WriteLine($"Release Year: {play.ReleaseYear}");
